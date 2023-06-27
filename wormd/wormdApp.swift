8a92f17878487ab13224e372382bd5abd30a3818
Olly <makrueger208@gmail.com>
2023-06-27 15:44:34 -0600

    Initial Commit

---
+++ wormd/wormdApp.swift
@@ -0,0 +1,17 @@
+//
+//  wormdApp.swift
+//  wormd
+//
+//  Created by Scholar on 6/27/23.
+//
+
+import SwiftUI
+
+@main
+struct wormdApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
